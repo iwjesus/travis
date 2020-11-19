89fa16ce0ab76367f257b2cfd90b595ced6e7264
Irlani Jesus <irlani.wilson@gmail.com>
2020-11-19 08:06:30 -0300

    Alterando teste

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Testes automatizados na pipeline com Travis CI.")
 
 
 if __name__ == "__main__":
